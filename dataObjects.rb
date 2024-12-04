6739437348bcd2e503e6e06896dc58496337d510
elkayy <63310939+imelkayy@users.noreply.github.com>
2024-12-04 13:55:48 -0800

    Create Geometry and Properties data classes

--- dataObjects.rb
+++ dataObjects.rb
@@ -19,3 +19,50 @@ class GisJsonObj
   end
 
 end
+
+class ChildData
+
+  def to_hash()
+    @data.compact!
+    return @data
+  end
+
+end
+
+class Geometry < ChildData
+
+  def initialize(type: nil, coordinates: [])
+    @data = {
+      type: @type,
+      coordinates: @coordinates
+    }
+  end
+
+  def set_type(type)
+    @data[:type] = type
+  end
+
+  def set_coordinates(coordinates)
+    @data[:coordinates] = coordinates
+  end
+
+end
+
+class Properties < ChildData
+
+  def initialize(title: nil, icon: nil)
+    @data = {
+      title: title,
+      icon: icon
+    }
+  end
+
+  def set_title(title)
+    @data[:title] = title
+  end
+
+  def set_icon(icon)
+    @data[:icon] = icon
+  end
+
+end
