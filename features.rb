1644702bed62787d985aa7b4e12e99df94c2903c
elkayy <63310939+imelkayy@users.noreply.github.com>
2024-12-04 15:22:18 -0800

    Create create_json_obj() function in Feature class

--- features.rb
+++ features.rb
@@ -1,6 +1,17 @@
 class Feature
 
   TYPE = "Feature"
+  GEO_TYPE = ""
+
+  def create_json_obj()
+    data = GisJsonObj.new(
+      type: TYPE,
+      properties: Properties.new(title: @name, icon: @type).to_hash(),
+      geometry: Geometry.new(type: self.class::GEO_TYPE, coordinates: @coordinates).to_hash()
+    )
+
+    return data
+  end
 
   def get_hash()
     return create_json_obj().to_hash()
