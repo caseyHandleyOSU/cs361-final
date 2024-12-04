17d6313bb17e3810b94161977bc22a781c3b18a8
elkayy <63310939+imelkayy@users.noreply.github.com>
2024-12-04 12:57:52 -0800

    Moved Point, Waypoint classes to locations.rb

---
+++ locations.rb
@@ -0,0 +1,57 @@
+class Point
+  attr_reader :lat, :lon, :ele
+
+  def initialize(lon, lat, ele=nil)
+    @lon = lon
+    @lat = lat
+    @ele = ele
+  end
+
+  def to_arr
+    arr = [lon, lat]
+    if ele != nil
+      arr.append(ele)
+    end
+
+    return arr
+  end
+
+end
+
+class Waypoint
+  attr_reader :name, :type
+
+  TYPE = "Feature"
+  GEO_TYPE = "Point"
+
+  def initialize(lon, lat, ele=nil, name=nil, type=nil)
+    @point = Point.new(lon, lat, ele) # Dependency... should this be passed in instead?
+    @name = name
+    @type = type
+  end
+
+  def lat
+    return @point.lat
+  end
+
+  def lon
+    return @point.lon
+  end
+
+  def ele
+    return @point.ele
+  end
+
+  def get_hash()
+    data = GisJsonObj.new(
+      type: TYPE,
+      properties: GisJson.properties(title: @name, icon: @type),
+      geometry: GisJson.geometry(type: GEO_TYPE, coordinates: @point.to_arr)
+    )
+    return data.get_hash()
+  end
+
+  def get_json(indent=0)
+    return GisJson.gen(get_hash())
+  end
+end
