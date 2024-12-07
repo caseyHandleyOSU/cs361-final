2013f60484f8c67c4cf326cc78478f7d8ce6b396
elkayy <63310939+imelkayy@users.noreply.github.com>
2024-12-07 09:45:28 -0800

    Fix main() function

--- gis.rb
+++ gis.rb
@@ -49,16 +49,16 @@ def main()
   w = Waypoint.new(point: Point.new(-121.5, 45.5, 30), name: "home", type: "flag")
   w2 = Waypoint.new(point: Point.new(-121.5, 45.6), name: "store", type: "dot")
 
-  ts1 = [ Point.new(-122, 45), Point.new(-122, 46), Point.new(-121, 46), ]
-  ts2 = [ Point.new(-121, 45), Point.new(-121, 46), ]
-  ts3 = [ Point.new(-121, 45.5), Point.new(-122, 45.5), ]
+  ts1 = PointSegment.new(points: [Point.new(-122, 45), Point.new(-122, 46), Point.new(-121, 46)])
+  ts2 = PointSegment.new(points: [Point.new(-121, 45), Point.new(-121, 46)])
+  ts3 = PointSegment.new(points: [Point.new(-121, 45.5), Point.new(-122, 45.5)])
 
   t = Track.new([ts1, ts2], "track 1")
   t2 = Track.new([ts3], "track 2")
 
   world = FeatureCollection.new("My Data", [w, w2, t, t2])
 
-  puts world.to_geojson()
+  puts world.to_json()
 
 end
 
